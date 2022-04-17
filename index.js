06b16acd218a68e3df9b5f9603e6bff83c97db14
tjs6595 <t.schultejans@schenckprocess.com>
2022-04-16 21:45:31 -0500

    This is my Web Game Part 4 Submission without Bonus

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ move(inventory).to(0, 0)
 
 const character = newImage('assets/green-character/static.gif')
 
-function handleDirectionChange(){
+function handleDirectionChange(direction){
     if(direction === null){
         character.src = 'assets/green-character/static.gif'
     }

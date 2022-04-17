09d44dfa2c8da807a580f85ebe61fe5fa48ace35
tjs6595 <t.schultejans@schenckprocess.com>
2022-04-16 21:23:35 -0500

    My working code down to step 4.

--- index.js
+++ index.js
@@ -2,10 +2,34 @@ const inventory = newInventory()
 move(inventory).to(0, 0)
 
 const character = newImage('assets/green-character/static.gif')
-move(character).to(100, 250)
+
+function handleDirectionChange(){
+    if(direction === null){
+        character.src = 'assets/green-character/static.gif'
+    }
+    if(direction === 'west'){
+        character.src = 'assets/green-character/west.gif'
+    }
+    if(direction === 'north'){
+        character.src = 'assets/green-character/north.gif'
+    }
+    if(direction === 'east'){
+        character.src = 'assets/green-character/east.gif'
+    }
+    if(direction === 'south'){
+        character.src = 'assets/green-character/south.gif'
+    }
+}
+
+move(character).withArrowKeys(100, 250, handleDirectionChange)
+
+
 
 
 move(newImage('assets/tree.png')).to(200, 450)
+//move(newImage('assets/tree.png')).withArrowKeys(200, 450)
+
+
 move(newImage('assets/pillar.png')).to(350, 250)
 move(newImage('assets/pine-tree.png')).to(450, 350)
 move(newImage('assets/crate.png')).to(150, 350)
